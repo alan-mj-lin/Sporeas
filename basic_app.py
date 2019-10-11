2950785b5a2c424bcc733fab9c7b481a662c0dfe
Alan Lin <alan.mj.lin@gmail.com>
2019-10-11 17:09:08 -0400

    Changes for refreshing projection on form update
    - added socketIO-flask plugin functionality
    - client-side javascript added for index and form html

--- basic_app.py
+++ basic_app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 title = "Title"
 hymn = "Hymn"
@@ -9,26 +12,23 @@ verse = "Verse"
 
 @app.route('/')
 def index():
-    global formRequest
-    if formRequest:
-        redirect(url_for('index'), code=302)
-        formRequest = False
     return render_template("index.html", titleString=title, hymnString=hymn, verseString=verse)
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
+    return render_template("form.html")
+
+@socketio.on('my broadcast event', namespace='/')
+def test_message(message):
     global title
     global hymn
     global verse
 
-    if request.method == 'POST':
-        result = request.form
-        title = result.get('title')
-        hymn = result.get('hymn')
-        verse = result.get('verse')
-    return render_template("form.html")
-
+    title = message['title']
+    hymn = message['hymn']
+    verse = message['verse']
+    emit('refresh', namespace='/', broadcast=True)
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', 9000, debug=True)
+    socketio.run(app)
