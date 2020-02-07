49b2aef85cc0558cfb721df6059bbd59b608459f
hchiam <hchiam@gmail.com>
2020-02-07 12:12:52 -0500

    highlight button/inputbox with jquery-ui

--- static/welcome-overlay/welcome-overlay.js
+++ static/welcome-overlay/welcome-overlay.js
@@ -4,42 +4,52 @@ $('button#highlight-service').click(function() {
   $('#welcome-overlay').hide();
   const active = sessionStorage.getItem('user');
   if (!active) return; // avoid showing tab when user is not logged in
-  document.querySelector('a[data-tab="main"]').click();
+  $('a[data-tab="main"]').click();
+  $('input#title').effect('highlight', {color: 'blue'}, 1000);
 });
 
 $('button#highlight-hymn-singing').click(function() {
   $('#welcome-overlay').hide();
   const active = sessionStorage.getItem('user');
   if (!active) return; // avoid showing tab when user is not logged in
-  document.querySelector('a[data-tab="utility"]').click();
+  $('a[data-tab="utility"]').click();
+  $('#hymn_input').effect('highlight', {color: 'blue'}, 1000);
+  $('button#hymn_singing').effect('shake', 1000);
 });
 
 $('button#highlight-morning-prayer').click(function() {
   $('#welcome-overlay').hide();
   const active = sessionStorage.getItem('user');
   if (!active) return; // avoid showing tab when user is not logged in
-  document.querySelector('a[data-tab="utility"]').click();
+  $('a[data-tab="utility"]').click();
+  $('#m_hymn_input').effect('highlight', {color: 'blue'}, 1000);
+  $('button#morning_prayer').effect('shake', 1000);
 });
 
 $('button#highlight-announcements').click(function() {
   $('#welcome-overlay').hide();
   const active = sessionStorage.getItem('user');
   if (!active) return; // avoid showing tab when user is not logged in
-  document.querySelector('a[data-tab="announcements"]').click();
+  $('a[data-tab="announcements"]').click();
+  $('#GA').effect('highlight', {color: 'blue'}, 1000);
 });
 
 $('button#highlight-holy-communion').click(function() {
   $('#welcome-overlay').hide();
   const active = sessionStorage.getItem('user');
   if (!active) return; // avoid showing tab when user is not logged in
-  document.querySelector('a[data-tab="utility"]').click();
+  $('a[data-tab="utility"]').click();
+  $('#hf_hymn_input').effect('highlight', {color: 'blue'}, 1000);
+  $('button#holy_communion').effect('shake', 1000);
 });
 
 $('button#highlight-foot-washing').click(function() {
   $('#welcome-overlay').hide();
   const active = sessionStorage.getItem('user');
   if (!active) return; // avoid showing tab when user is not logged in
-  document.querySelector('a[data-tab="utility"]').click();
+  $('a[data-tab="utility"]').click();
+  $('#hf_hymn_input').effect('highlight', {color: 'blue'}, 1000);
+  $('button#foot_washing').effect('shake', 1000);
 });
 
 // hide welcome overlay in this session if user asks to:
@@ -49,8 +59,6 @@ if (showWelcomeOverlay) {
 } else {
   $('#welcome-overlay').hide();
 }
-console.log(showWelcomeOverlay)
-console.log(sessionStorage.getItem('showWelcomeOverlay') === 'true')
 $('button#stop-showing-welcome-overlay').click(function() {
   $('#welcome-overlay').hide();
   showWelcomeOverlay = false;
