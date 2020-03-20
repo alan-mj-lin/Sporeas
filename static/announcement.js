ece839b36cdcd7eed7ee56d264ba9e89fc66543f
hchiam <hchiam@gmail.com>
2020-03-19 20:34:20 -0400

    for #81: simplify: edit/lock -> just edit

--- static/announcement.js
+++ static/announcement.js
@@ -17,28 +17,23 @@ $(document).ready(function() {
 
     $('#update').click(function() {
         $("p").attr('contenteditable', 'false');
+        sessionStorage.setItem('contenteditable', false);
         localStorage.setItem(user+'_ann', $('#announcements').html());
         console.log(user);
         socket.emit('update announcement', {room: user});
         $('.lock.icon:not(.open)').show();
         $('.lock.open.icon').hide();
-        sessionStorage.setItem('contenteditable', false);
     });
 
     $('#edit').click(function() {
         const get_announcement = localStorage.getItem(user+ '_ann');
-        const contenteditable = sessionStorage.getItem('contenteditable');
-        if (get_announcement != null) {
+        const contentIsLocked = sessionStorage.getItem('contenteditable') !== 'true';
+        if (get_announcement != null && contentIsLocked) {
             $('#announcements').html(get_announcement);
-            if (contenteditable && contenteditable === 'false') {
-                $("p").attr('contenteditable', 'true');
-                sessionStorage.setItem('contenteditable', true);
-            } else {
-                $("p").attr('contenteditable', 'false');
-                sessionStorage.setItem('contenteditable', false);
-            }
+            $("p").attr('contenteditable', 'true');
+            sessionStorage.setItem('contenteditable', true);
         }
-        $('.lock.icon:not(.open)').toggle();
-        $('.lock.open.icon').toggle();
+        $('.lock.icon:not(.open)').hide();
+        $('.lock.open.icon').show();
     });
 });
