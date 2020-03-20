8dda5cd11d3c14cf05edea9a78329c883708e05b
hchiam <hchiam@gmail.com>
2020-03-19 20:23:13 -0400

    for #81: (un)locked toggles contenteditable too

--- static/announcement.js
+++ static/announcement.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     const protocol = window.location.protocol;
     const socket = io.connect(
       protocol + '//' + document.domain + ':' + location.port
@@ -7,30 +7,38 @@ $(document).ready(function(){
     user = user.substr(1);
     user = user.split('_')[0]
     let announcement = localStorage.getItem(user+ '_ann');
-    if (announcement != null){
+    if (announcement != null) {
         $('#announcements').html(announcement);
         $("p").attr('contenteditable','true');
     }
 
-    $('#update').click(function(){
+    $('.lock.icon:not(.open)').hide();
+    $('.lock.open.icon').show();
+
+    $('#update').click(function() {
         $("p").attr('contenteditable', 'false');
         localStorage.setItem(user+'_ann', $('#announcements').html());
         console.log(user);
         socket.emit('update announcement', {room: user});
         $('.lock.icon:not(.open)').show();
         $('.lock.open.icon').hide();
+        sessionStorage.setItem('contenteditable', false);
     });
 
-    $('#edit').click(function(){
-        let get_announcement = localStorage.getItem(user+ '_ann');
-        if (get_announcement != null){
+    $('#edit').click(function() {
+        const get_announcement = localStorage.getItem(user+ '_ann');
+        const contenteditable = sessionStorage.getItem('contenteditable');
+        if (get_announcement != null) {
             $('#announcements').html(get_announcement);
-            $("p").attr('contenteditable','true');
+            if (contenteditable && contenteditable === 'false') {
+                $("p").attr('contenteditable', 'true');
+                sessionStorage.setItem('contenteditable', true);
+            } else {
+                $("p").attr('contenteditable', 'false');
+                sessionStorage.setItem('contenteditable', false);
+            }
         }
         $('.lock.icon:not(.open)').toggle();
         $('.lock.open.icon').toggle();
     });
-
-    $('.lock.icon:not(.open)').hide();
-    $('.lock.open.icon').show();
 });
